17da7ea35829e6b8ef8a6e4070adf0f38f992a59
brandonxiang <1542453460@qq.com>
2016-07-22 15:08:51 +0800

    s4

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,22 @@
 <template>
-<div class="message">{{ msg }}</div>
+<div class="message">Value is: {{ count }}</div>
+<a href="#" @click.prevent="increment">Increment</a>
 </template>
 
 <script>
 export default {
- data () {
- return {
- msg: 'Hello from vue-loader!'
- }
- }
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment () {
+      this.count ++;
+    }
+  }
 }
 </script>
 
 <style>
-.message {
- color: blue;
-}
 </style>
