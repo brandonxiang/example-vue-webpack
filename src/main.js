b580066dbd75786231e4e3e4aa6806a065897649
brandonxiang <1542453460@qq.com>
2016-07-22 13:02:47 +0800

    step one and two

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from 'vue'
 
-var example1 = new Vue({
+new Vue({
   el: 'body',
-  components: { App }
-});
+  data: {
+      message: "Hello Vue"
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,24 @@
-var path = require('path')
-var webpack = require('webpack')
-
 module.exports = {
+  // 这是一个主文件包括其他模块
   entry: './src/main.js',
+  // 编译的文件路径
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+      //`dist`文件夹
+    path: './dist',
+    // 文件 `build.js` 即 dist/build.js
     filename: 'build.js'
   },
-  resolveLoader: {
-    root: path.join(__dirname, 'node_modules'),
-  },
   module: {
+    // 一些特定的编译规则
     loaders: [
       {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
-      {
+        // 让webpack去验证文件是否是.js结尾将其转换
         test: /\.js$/,
+        // 通过babel转换
         loader: 'babel',
+        // 不用转换的node_modules文件夹
         exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      {
-        test: /\.html$/,
-        loader: 'vue-html'
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url',
-        query: {
-          limit: 10000,
-          name: '[name].[ext]?[hash]'
-        }
       }
     ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  devtool: '#eval-source-map'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vuejs.github.io/vue-loader/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.optimize.OccurenceOrderPlugin()
-  ])
+  }
 }
